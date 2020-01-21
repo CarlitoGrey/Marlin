3e0075e5b5ad4681d2dd210858dbdd1f6cb6f8da
CarlitoGrey <mandeepb@gmail.com>
2020-01-21 22:29:21 +0000

    Change Z-Offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -931,7 +931,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET {  -17, -45, 0 }
+#define NOZZLE_TO_PROBE_OFFSET {  -17, -45, 0.92 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
